418162b1f58a87cead8e9a7abf990747b0e33376
Antonio <antoniotmll@gmail.com>
2022-10-26 22:44:16 +0200

    added new definition

--- back/Database/Repositories/IUserRepository.cs
+++ back/Database/Repositories/IUserRepository.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using back.Database.Models;
 
 namespace back.Database.Repositories
 {
     public interface IUserRepository
     {
-        
+        public Task CreateMember(CreateUserDb request);
     }
 }
